1734b13945afe0cb18dea24f6073ff4bae1a6d89
hito-kotaro <tohi.work@gmail.com>
2022-02-17 11:06:57 +0900

    first commit

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+STACK_NAME=aurora-control-lambda
+TEMPLATE_FILE=./aurora-control-lambda.yml
+
+aws cloudformation deploy \
+    --template-file $TEMPLATE_FILE \
+    --stack-name $STACK_NAME \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides `cat parameters.properties` 
